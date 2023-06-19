1a7906b89bbff6a78627c4acf1f04cf143876dd9
qprigent <qprigent@campus-eni.fr>
2023-06-19 11:54:59 +0200

    Module 6.

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
 use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -30,6 +35,30 @@ class WishController extends AbstractController
         $wish = $wishRepository->find($id);
         return $this->render('wish/details.html.twig', [
             'wish' => $wish,
+        ]);
+    }
+
+    #[Route('/create', name: 'create')]
+    public function create(EntityManagerInterface $entityManager, Request $request): Response {
+        $wish = new Wish();
+        $wishForm = $this->createForm(WishType::class, $wish);
+        $wishForm->handleRequest($request);
+
+        if ($wishForm->isSubmitted() && $wishForm->isValid()) {
+            $wish->setIsPublished(true);
+            $wish->setDateCreated(new \DateTime());
+
+            try {
+                $entityManager->persist($wish);
+                $entityManager->flush();
+                $this->addFlash('success', 'Le souhait a bien été enregistré.');
+            } catch(Exception $exception) {
+                $this->addFlash('danger', 'Le souhait n\'a pas été enregistré.');
+            }
+        }
+
+        return $this->render('wish/create.html.twig', [
+            'wishForm' => $wishForm->createView()
         ]);
     }
 }
